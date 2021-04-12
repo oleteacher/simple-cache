0971c47cbcedcd5705268e5305150fc6500c9a76
Susan <oleteacher@users.noreply.github.com>
2021-04-11 20:24:11 -0500

    Update example.php

--- example.php
+++ example.php
@@ -4,6 +4,33 @@ include('top-cache.php');
 
 // Your page content
 
+<h1>Bring her forward!</h1>
+<p>Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! He hasn't got shit all over him. And this isn't my nose. This is a false one. Camelot!</p>
+<p>We want a shrubbery!! I dunno. <strong> Must be a king.</strong> <em> Bloody Peasant!</em> Ah, now we see the violence inherent in the system!</p>
+<h2>Now, look here, my good man.</h2>
+<p>Listen. Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. You can't expect to wield supreme power just 'cause some watery tart threw a sword at you!</p>
+<ol>
+<li>Why do you think that she is a witch?</li><li>Well, how'd you become king, then?</li><li>Well, we did do the nose.</li>
+</ol>
+
+<h3>Well, we did do the nose.</h3>
+<p>How do you know she is a witch? What do you mean? Now, look here, my good man. Well, we did do the nose. I dunno. Must be a king.</p>
+<ul>
+<li>Oh, ow!</li><li>Well, what do you want?</li><li>She looks like one.</li>
+</ul>
+
+<p>I have to push the pram a lot. Knights of Ni, we are but simple travelers who seek the enchanter who lives beyond these woods. Oh! Come and see the violence inherent in the system! Help, help, I'm being repressed!</p>
+<p>Bring her forward! I have to push the pram a lot. We want a shrubbery!! It's only a model.</p>
+<p>Well, we did do the nose. Shut up! Will you shut up?! Well, she turned me into a newt. Why do you think that she is a witch? What a strange person.</p>
+<p>On second thoughts, let's not go there. It is a silly place. A newt? And this isn't my nose. This is a false one. What do you mean? Bloody Peasant! A newt?</p>
+<p>Well, how'd you become king, then? We shall say 'Ni' again to you, if you do not appease us. And the hat. She's a witch! Found them? In Mercia?! The coconut's tropical! Knights of Ni, we are but simple travelers who seek the enchanter who lives beyond these woods.</p>
+<p>Bring her forward! On second thoughts, let's not go there. It is a silly place. Listen. Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.</p>
+<p>Camelot! I'm not a witch. Well, how'd you become king, then? The swallow may fly south with the sun, and the house martin or the plover may seek warmer climes in winter, yet these are not strangers to our land.</p>
+<p>The nose? He hasn't got shit all over him. The swallow may fly south with the sun, and the house martin or the plover may seek warmer climes in winter, yet these are not strangers to our land. I'm not a witch.</p>
+<p>Did you dress her up like this? Shut up! How do you know she is a witch? We want a shrubbery!!</p>
+<p>I have to push the pram a lot. The Lady of the Lake, her arm clad in the purest shimmering samite, held aloft Excalibur from the bosom of the water, signifying by divine providence that I, Arthur, was to carry Excalibur. That is why I am your king.</p>
+<p>Ah, now we see the violence inherent in the system! We found them. He hasn't got shit all over him. And the hat. She's a witch! The Knights Who Say Ni demand a sacrifice!</p>
+
 include('bottom-cache.php');
 
 ?>
